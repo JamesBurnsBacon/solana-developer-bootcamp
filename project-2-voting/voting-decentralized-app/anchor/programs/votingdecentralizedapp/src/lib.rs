77ba6c89344db6104934346635b7405aa671fc41
JamesBurnsBacon <cookingbaconwithfire@gmail.com>
2025-01-09 23:53:54 -0500

    Project 2 progress

--- project-2-voting/voting-decentralized-app/anchor/programs/votingdecentralizedapp/src/lib.rs
+++ project-2-voting/voting-decentralized-app/anchor/programs/votingdecentralizedapp/src/lib.rs
@@ -7,4 +7,46 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 #[program]
 pub mod votingdecentralizedapp {
     use super::*;
+
+    pub fn initialize_poll(context: Context<InitializePoll>,
+                            poll_id: u64,
+                            description: String,
+                            poll_start: u64,
+                            poll_end: u64 ) -> Result<()> {
+        let poll = mut context.accounts.poll;
+        poll.poll_id = poll_id;      
+        poll.description = description;
+        poll.poll_start = poll_start;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;                  
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init, //does create account
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE, //Anchor gets its 8 reserved bytes
+        seeds = [poll_id.to_le_bytes().as_ref()], 
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64, //unix timestamps
+    pub poll_end: u64,
+    pub candidate_amount: u64,
 }
--- project-2-voting/voting-decentralized-app/anchor/tests/votingdecentralizedapp.spec.ts
+++ project-2-voting/voting-decentralized-app/anchor/tests/votingdecentralizedapp.spec.ts
@@ -1,76 +1,11 @@
 import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
 import {Keypair} from '@solana/web3.js'
-import {Votingdecentralizedapp} from '../target/types/votingdecentralizedapp'
 
-describe('votingdecentralizedapp', () => {
+describe('Voting', () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
 
-  const program = anchor.workspace.Votingdecentralizedapp as Program<Votingdecentralizedapp>
-
-  const votingdecentralizedappKeypair = Keypair.generate()
-
-  it('Initialize Votingdecentralizedapp', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        votingdecentralizedapp: votingdecentralizedappKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([votingdecentralizedappKeypair])
-      .rpc()
-
-    const currentCount = await program.account.votingdecentralizedapp.fetch(votingdecentralizedappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(0)
-  })
-
-  it('Increment Votingdecentralizedapp', async () => {
-    await program.methods.increment().accounts({ votingdecentralizedapp: votingdecentralizedappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdecentralizedapp.fetch(votingdecentralizedappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Increment Votingdecentralizedapp Again', async () => {
-    await program.methods.increment().accounts({ votingdecentralizedapp: votingdecentralizedappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdecentralizedapp.fetch(votingdecentralizedappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement Votingdecentralizedapp', async () => {
-    await program.methods.decrement().accounts({ votingdecentralizedapp: votingdecentralizedappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdecentralizedapp.fetch(votingdecentralizedappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set votingdecentralizedapp value', async () => {
-    await program.methods.set(42).accounts({ votingdecentralizedapp: votingdecentralizedappKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votingdecentralizedapp.fetch(votingdecentralizedappKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the votingdecentralizedapp account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        votingdecentralizedapp: votingdecentralizedappKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.votingdecentralizedapp.fetchNullable(votingdecentralizedappKeypair.publicKey)
-    expect(userAccount).toBeNull()
+  it('Initialize Poll', async () => {
+    
   })
 })
