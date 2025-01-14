91961f48f07a8b1eb3b3a7e8d497da0311510345
JamesBurnsBacon <cookingbaconwithfire@gmail.com>
2025-01-14 15:09:34 -0500

    Project 6 progress

---
+++ project-6-nfts/new-nft/create-collection.ts
@@ -0,0 +1,41 @@
+import { createNft, fetchDigitalAsset, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { airdropIfRequired, getExplorerLink, getKeypairFromFile } from "@solana-developers/helpers";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { generateSigner, keypairIdentity, percentAmount } from "@metaplex-foundation/umi";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+const user = await getKeypairFromFile(); //unspecified will use the id.json in home dir
+
+await airdropIfRequired(connection, user.publicKey, 1 * LAMPORTS_PER_SOL, 0.5 * LAMPORTS_PER_SOL); //adds devnet sol
+
+console.log("Loaded user", user.publicKey.toBase58());
+
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata()); //tools we want to use
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey); //umi version of user
+umi.use(keypairIdentity(umiUser));
+
+console.log("Set up Umi instance for user");
+
+const collectionMint = generateSigner(umi); //making a keypair
+
+const transaction = await createNft(umi, {
+    mint: collectionMint,
+    name: "My Collection",
+    symbol: "MC",
+    uri: "https://...",
+    sellerFeeBasisPoints: percentAmount(0), //artists can make money on secondary sales with this
+    isCollection: true
+}); //creates the nft collection
+
+await transaction.sendAndConfirm(umi);
+
+const createdCollectionNft = await fetchDigitalAsset(umi, collectionMint.publicKey);
+
+console.log(`Created Collection MC! Address is ${getExplorerLink(
+    "address",
+    createdCollectionNft.mint.publicKey, 
+    "devnet"
+)}`)
