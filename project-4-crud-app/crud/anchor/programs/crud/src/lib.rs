f4c1681cd8ddbfa6cd50763c85967507eaae5a82
JamesBurnsBacon <cookingbaconwithfire@gmail.com>
2025-01-17 16:29:02 -0500

    Project 4 progress

--- project-4-crud-app/crud/anchor/programs/crud/src/lib.rs
+++ project-4-crud-app/crud/anchor/programs/crud/src/lib.rs
@@ -5,44 +5,90 @@ use anchor_lang::prelude::*;
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod crud {
+pub mod counter {
     use super::*;
 
+    pub fn create_journal_entry(context: Context<CreateEntry>, title: String, message: String) -> Result<()> {
+      let journal_entry = &mut context.accounts.journal_entry;
+      journal_entry.owner = *context.accounts.owner.key;
+      journal_entry.title = title;
+      journal_entry.message = message;
+      Ok(())
+    }
+
+    pub fn update_journal_entry(context: Context<UpdateEntry>, _title: String, message: String) -> Result<()> {
+      let journal_entry = &mut context.accounts.journal_entry;
+      journal_entry.message = message;
+      Ok(())
+    }
+
+    pub fn delete_journal_entry(_context: Context<DeleteEntry>, _title: String) -> Result <()> {
+      Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeCrud<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
   #[account(
-  init,
-  space = 8 + Crud::INIT_SPACE,
-  payer = payer
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()], //must have two seeds or more, not just owner
+    bump, //needed bc we defined a seed
+    space = 8 + JournalEntryState::INIT_SPACE,
+    payer = owner,
   )]
-  pub crud: Account<'info, Crud>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCrud<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
 
+  pub journal_entry: Account<'info, JournalEntryState>,
+
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateEntry<'info> {
   #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
+    mut,
+    seeds = [title.as_bytes(), owner.key().as_ref()], //must have two seeds or more, not just owner
+    bump, //needed bc we defined a seed
+    realloc = 8 + JournalEntryState::INIT_SPACE,
+    realloc::payer = owner,
+    realloc::zero = true,
   )]
-  pub crud: Account<'info, Crud>,
+
+  pub journal_entry: Account<'info, JournalEntryState>,
+
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+  #[account(
+    mut,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    close = owner, //only closes if the pubkey matches
+  )]
+  pub journal_entry: Account<'info, JournalEntryState>,
+
   #[account(mut)]
-  pub crud: Account<'info, Crud>,
+  pub owner: Signer<'info>,
+
+  pub system_program: Program<'info, System>
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Crud {
-  count: u8,
+pub struct JournalEntryState {
+  pub owner: Pubkey,
+  #[max_len(64)]
+  pub title: String,
+  #[max_len(1024)]
+  pub message: String,
 }
